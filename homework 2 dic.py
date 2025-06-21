d44334f4272df84af14243b623d91027ad5ab8fd
ytk-kayden <kaydenflynn2@gmail.com>
2025-06-21 12:30:53 +0100

    Add files via upload

---
+++ homework 2 dic.py
@@ -0,0 +1,7 @@
+textbook={"english":39,"maths":20,"german":87,"phy":56}
+textbook["phy"]=200
+textbook["woodwork"]=59
+textbook["irish"]=28
+x=input("witch book do you want the prize for?")
+print(textbook[x])
+print(textbook)
